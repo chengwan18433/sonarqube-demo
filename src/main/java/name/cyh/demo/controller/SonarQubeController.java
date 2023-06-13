98f40feb636c95389aea3b67eca9bf3c2afb48c4
chengyonghui <chengyonghui@zsoft.com.cn>
2023-06-13 10:38:09 +0800

    update

---
+++ src/main/java/name/cyh/demo/controller/SonarQubeController.java
@@ -0,0 +1,15 @@
+package name.cyh.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Administrator
+ */
+@RestController
+public class SonarQubeController {
+    @GetMapping("hello")
+    public String hello() {
+        return String.valueOf(System.currentTimeMillis());
+    }
+}
